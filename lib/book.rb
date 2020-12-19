a6a6d9592061c2d20242d0795fb65df44b3da259
calmpsycho333 <wrathking75@yahoo.com>
2020-12-19 14:33:02 -0500

    commit tree 6976f60155b18f36457d6e8d8c1bf9fd90653cd5

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,20 @@
+require 'pry' 
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
-
+#wtf 
+  GENRES = []
   def initialize(title)
     @title = title
   end
-
+#whats next crazy?
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+#wtf is coding
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
 end
